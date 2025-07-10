7759ba2e93a3428e8d6421c3c1480d53a2251b3c
noboKumar <nobokumar227@gmail.com>
2025-07-10 21:21:24 +0600

    add partnership marquee

---
+++ src/components/sections/Home/PartnershipSection.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import coursera from "../../../assets/Coursera.png";
+import edx from "../../../assets/edx.png";
+import udemy from "../../../assets/udemy.png";
+import freeCodeCamp from "../../../assets/freeCodeCamp.jpeg";
+import khanAcademy from "../../../assets/khanAcademy.png";
+import skillshare from "../../../assets/skill-share.png";
+import Marquee from "react-fast-marquee";
+
+const partnerIcons = [
+  coursera,
+  edx,
+  udemy,
+  freeCodeCamp,
+  khanAcademy,
+  skillshare,
+];
+const marqueeItems = [
+  ...partnerIcons,
+  ...partnerIcons,
+  ...partnerIcons,
+  ...partnerIcons,
+];
+
+const PartnershipSection = () => {
+  return (
+    <div className="relative py-10">
+      {/* Left fade */}
+      <div className="absolute left-0 top-0 h-full w-20 bg-gradient-to-r from-white dark:from-base-100 to-transparent z-10 pointer-events-none"></div>
+
+      {/* Right fade */}
+      <div className="absolute right-0 top-0 h-full w-20 bg-gradient-to-l from-white dark:from-base-100 to-transparent z-10 pointer-events-none"></div>
+
+      {/* Marquee */}
+      <Marquee speed={50} gradient={false}>
+        <div className="flex gap-10 items-center">
+          {marqueeItems.map((icon, index) => (
+            <img
+              key={index}
+              src={icon}
+              alt={`Partner ${index + 1}`}
+              className="w-20 rounded-xl grayscale hover:grayscale-0 transition duration-300 ease-in-out cursor-pointer"
+            />
+          ))}
+        </div>
+      </Marquee>
+    </div>
+  );
+};
+
+export default PartnershipSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BannerSection from '../components/sections/Home/BannerSection';
+import PartnershipSection from '../components/sections/Home/PartnershipSection';
 
 const Home = () => {
     return (
         <div>
             <BannerSection></BannerSection>
+            <PartnershipSection></PartnershipSection>
         </div>
     );
 };
