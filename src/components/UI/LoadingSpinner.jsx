1f8a565dedba6a64acec48b795cb4441caa6609c
noboKumar <nobokumar227@gmail.com>
2025-07-12 00:58:44 +0600

    update loading spinner

--- src/components/UI/LoadingSpinner.jsx
+++ src/components/UI/LoadingSpinner.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LoadingSpinner = () => {
-  return <span className="loading loading-spinner loading-xl"></span>;
+  return (
+    <div className="flex items-center justify-center my-50">
+      <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
