a56ba692a2025fccf81ca7cb8b72458fdfee0e53
noboKumar <nobokumar227@gmail.com>
2025-07-16 05:41:29 +0600

    fix dashboard nav

--- src/components/Dashboard/DashboardNavbar.jsx
+++ src/components/Dashboard/DashboardNavbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ToggleTheme from "../UI/ToggleTheme";
 import useAuth from "../../hooks/useAuth";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const DashboardNavbar = () => {
   const { user } = useAuth();
   return (
     <div className="bg-base-100 shadow-sm border-b-2 border-gray-300 sticky top-0 z-40">
       <div className="flex justify-between items-center p-4 w-11/12 mx-auto">
+        {/* Hamburger menu - only visible on medium/small screens */}
+        <div className="md:hidden">
+          <label
+            htmlFor="responsive-drawer"
+            className="btn btn-ghost drawer-button"
+          >
+            <GiHamburgerMenu className="text-xl" />
+          </label>
+        </div>
         <h1 className="text-3xl font-bold my-4">Dashboard</h1>
         <div className="flex items-center gap-4">
           <ToggleTheme />
--- src/layouts/DashBoardLayout.jsx
+++ src/layouts/DashBoardLayout.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
 import { Outlet } from "react-router";
 import DashboardSideBar from "../components/Dashboard/DashboardSideBar";
 import DashboardNavbar from "../components/Dashboard/DashboardNavbar";
@@ -13,15 +12,7 @@ const DashBoardLayout = () => {
       {/* Main Content Area */}
       <div className="drawer-content flex flex-col">
         <DashboardNavbar></DashboardNavbar>
-        {/* Hamburger menu - only visible on medium/small screens */}
-        <div className="p-4 md:hidden">
-          <label
-            htmlFor="responsive-drawer"
-            className="btn btn-primary drawer-button"
-          >
-            <GiHamburgerMenu className="text-xl" />
-          </label>
-        </div>
+
 
         {/* Your page content here */}
         <div className="p-4 w-11/12 mx-auto">
